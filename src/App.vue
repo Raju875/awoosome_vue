1a7125862738cd43dca1cd0321430c7982825213
Raju875 <alaminraju875@gmail.com>
2022-12-30 23:05:03 +0600

    Vue component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,19 @@
   <div>
     <h3>
       {{ msg }}
+      <Contact></Contact>
     </h3>
   </div>
 </template>
 
 <script>
+import ContactDetails from "./ContactDetails.vue";
+
 export default {
+  components: {
+    Contact: ContactDetails,
+  },
+
   data() {
     return {
       msg: "Hi! I am vue cli.",
---
+++ src/ContactDetails.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <li v-for="info in infos" :key="info.name">
+      <ol>
+        {{
+          info.name
+        }}
+      </ol>
+      <ol>
+        {{
+          info.age
+        }}
+      </ol>
+      <ol>
+        {{
+          info.occupation
+        }}
+      </ol>
+      <ol>
+        {{
+          info.phone
+        }}
+      </ol>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      infos: [
+        {
+          name: "Ali Azgar Rana",
+          age: 30,
+          occupation: "Businessman",
+          phone: "017",
+        },
+        {
+          name: "Al Amin Raju",
+          age: 29,
+          occupation: "Software Engineer",
+          phone: "016",
+        },
+        {
+          name: "Ariful Islam Raj",
+          age: 19,
+          occupation: "Student",
+          phone: "018",
+        },
+      ],
+    };
+  },
+};
+</script>
