7a4b9eb1805040f07373f1901bef01938869029a
Raju875 <alaminraju875@gmail.com>
2022-12-30 19:17:28 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>
+      {{ msg }}
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hi! I am vue cli.",
+    };
+  },
+};
+</script>
